9c23b0cd25c53b001257d75b53de6d73dad2d80e
richardmiller <rmiller@inviqa.com>
2017-09-21 20:48:42 +0100

    Use getenv not $_ENV

--- app/config/parameters.php
+++ app/config/parameters.php
@@ -1,6 +1,6 @@
 <?php
 
-$container->setParameter('base_url', $_ENV['BASE_URL']);
+$container->setParameter('base_url', getenv('BASE_URL'));
 $mysql = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $container->setParameter('database_driver', 'pdo_mysql');
@@ -10,6 +10,6 @@ $container->setParameter('database_user', $mysql['user']);
 $container->setParameter('database_password', $mysql['pass']);
 $container->setParameter('database_path', null);
 
-$container->setParameter('jwt_private_key_path', $_ENV['JWT_PRIVATE_KEY']);
-$container->setParameter('jwt_public_key_path', $_ENV['JWT_PUBLIC_KEY']);
-$container->setParameter('jwt_key_pass_phrase', $_ENV['JWT_PASS_PHRASE']);
+$container->setParameter('jwt_private_key_path', getenv('JWT_PRIVATE_KEY'));
+$container->setParameter('jwt_public_key_path', getenv('JWT_PUBLIC_KEY'));
+$container->setParameter('jwt_key_pass_phrase', getenv('JWT_PASS_PHRASE'));
